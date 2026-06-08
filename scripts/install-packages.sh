44e5d751e89c6768376bd9d4813a6ae58377b4b9
Luke Jacobs <lj15595@optonline.net>
2026-06-07 20:44:26 -0400

    rust support

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Installing Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+
 echo "Installing packages..."
 
 packages=()
